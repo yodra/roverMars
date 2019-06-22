6c4edc69d361ec84843052f3f677dfe419f51e4a
Maik <maikelreyes96@hotmail.com>
2019-06-22 11:51:46 +0100

    refactor terminal

--- src/Terminal.ts
+++ src/Terminal.ts
@@ -13,23 +13,22 @@ export class Terminal {
 
     private translate = (command: string) => {
         switch (command) {
-            case 'f': return {
-                execute: this.rover.forward,
-                help: () => "The robot move to front"
-            }
-            case 'b': return {
-                execute: this.rover.backward,
-                help: () => "The robot move to front"
-            }
-            case 'r': return {
-                execute: this.rover.turnRight,
-                help: () => "The robot move to front"
-            }
-            case 'l': return {
-                execute: this.rover.turnLeft,
-                help: () => "The robot move to front"
-            }
+            case 'f': 
+                return this.generate(this.rover.forward, "move to front")
+            case 'b': 
+                return this.generate(this.rover.backward, "move to back")
+            case 'r': 
+                return this.generate(this.rover.turnRight, "turn right 90 degrees") 
+            case 'l': 
+                return this.generate(this.rover.turnLeft, "turn left 90 degrees") 
             default: throw "Command not found"
+        }
+    }
+
+    private generate = (action: any, helpInfo: String) => {
+        return {
+            execute: action,
+            help: () => "The robot " + helpInfo.toLowerCase()
         }
     }
 }
